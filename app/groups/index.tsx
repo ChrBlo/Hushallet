3d1b478d354413903618b16c2744f8ea38950f38
SophWennersten <sophwennersten@hotmail.com>
2025-10-09 11:31:30 +0200

    Added taskButton with mocked data and styling

--- app/groups/index.tsx
+++ app/groups/index.tsx
@@ -1,24 +1,76 @@
+import { ScrollView, StyleSheet, Text } from 'react-native';
 import { MD3Theme, useTheme } from 'react-native-paper';
-import { HouseholdUser } from '../../types/household_user';
-import { ScrollView, StyleSheet } from 'react-native';
+import StyledButton from '../../components/styled-button';
+import TaskButton from '../../components/task-button';
 
 interface Household {
-  id?: string;
-  created_by: string;
+  id: string;
   name: string;
-  invitation_code: string;
-  users: HouseholdUser[];
 }
+
+//Temp data
+const houseHolds: Household[] = [
+  {
+    id: '001',
+    name: 'Familjen Annorlunda',
+  },
+  {
+    id: '002',
+    name: 'Bastun',
+  },
+];
 
 const GroupsScreen = () => {
   const theme = useTheme();
-  
-  <ScrollView>
+  const s = createStyles(theme);
 
-  </ScrollView>
+  return (
+    <>
+      <ScrollView contentContainerStyle={s.container}>
+        {houseHolds.map(h => (
+          <TaskButton key={h.id} onPress={() => {}}>
+            <Text style={s.text}>{h.name}</Text>
+          </TaskButton>
+        ))}
+      </ScrollView>
+      <StyledButton
+        title={'Lägg till'}
+        onPress={() => {}}
+        style={[s.button, s.bottomLeft]}
+      />
+      <StyledButton
+        title={'Ändra'}
+        onPress={() => {}}
+        style={[s.button, s.bottomRight]}
+      />
+    </>
+  );
 };
 
-const createStyles = (theme : MD3Theme) =>
+export default GroupsScreen;
+const createStyles = (theme: MD3Theme) =>
   StyleSheet.create({
-    
-  })
+    container: {
+      backgroundColor: theme.colors.background,
+      paddingTop: 10,
+      paddingBottom: 120,
+      gap: 10,
+    },
+    text: {
+      fontSize: 18,
+      fontWeight: '600',
+    },
+    bottomRight: {
+      position: 'absolute',
+      bottom: 30,
+      right: 0,
+    },
+    bottomLeft: {
+      position: 'absolute',
+      bottom: 30,
+      left: 0,
+    },
+    button: {
+      width: '42%',
+    },
+  });
