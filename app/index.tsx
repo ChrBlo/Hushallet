abf12403b734a77d3b488b2e1526ab0b2f88f1c7
Christine B <(none)>
2025-10-12 20:04:57 +0200

    added input fields and Login button

--- app/index.tsx
+++ app/index.tsx
@@ -1,11 +1,91 @@
 import { router } from "expo-router";
-import { Button, View } from "react-native";
+import { useState } from "react";
+import { Image, StyleSheet, View } from "react-native";
+import { MD3Theme, Text, TextInput, useTheme } from "react-native-paper";
+import StyledButton from '../components/styled-button';
 
 export default function LoginScreen() {
+  const theme = useTheme();
+  const s = createStyles(theme);
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const logoSource = theme.dark
+  ? require('../assets/images/logowhitebackground1.png')
+  : require('../assets/images/logoblackbackground.png');
 
   return (
-    <View>
-      <Button title="groups" onPress={() => router.push('/groups')} />
+
+    <View style={s.container}>
+      <View style={s.logoContainer}>
+        <Image source={logoSource} style={s.logo} resizeMode="contain"/>
+      </View>
+
+      <Text variant="headlineMedium" style={s.title}>
+        Logga in
+      </Text>
+
+      <TextInput
+        label="Användarnamn"
+        value={email}
+        onChangeText={setEmail}
+        mode="outlined"
+        activeOutlineColor={theme.colors.outline} 
+        autoCapitalize="none"
+        keyboardType="email-address"
+        style={s.input}
+      />
+
+      <TextInput
+        label="Lösenord"
+        value={password}
+        onChangeText={setPassword}
+        mode="outlined"
+        activeOutlineColor={theme.colors.outline} 
+        style={s.input}
+      />
+
+      <StyledButton
+        title="Logga in"
+        onPress={() => router.push('/groups')}
+        style={s.loginButton}
+      />
     </View>
-  )
+  );
 }
+
+const createStyles = (theme: MD3Theme) =>
+  StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: theme.colors.background,
+      justifyContent: "center",
+      padding: 40,
+    },
+    logoContainer: {
+      alignItems: "center",
+      marginBottom: 24,
+    },
+    logo: {
+      width: 150,
+      height: 150,
+      borderRadius: 20,
+      justifyContent: "center",
+      alignItems: "center",
+    },
+    title: {
+      textAlign: "center",
+      marginBottom: 24,
+    },
+    input: {
+      marginBottom: 16,
+      backgroundColor: theme.colors.surfaceVariant,
+    },
+    loginButton: {
+      marginTop: 8,
+    },
+    loginButtonContent: {
+      paddingVertical: 8,
+    },
+  });
