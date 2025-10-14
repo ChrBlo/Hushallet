3d4dc84260849255d00f458f73138c89be2eef6c
Sebastian <sebastian@sebastians.codes>
2025-10-14 17:03:00 +0200

    adding seeding function and changing the avatar creation

---
+++ components/avatar-bubble.tsx
@@ -0,0 +1,37 @@
+import { StyleProp, StyleSheet, Text, View, ViewStyle } from 'react-native';
+import type { AvatarConfig } from './get-avatar';
+
+type AvatarBubbleProps = {
+  config: AvatarConfig;
+  size?: number;
+  style?: StyleProp<ViewStyle>;
+};
+
+const AvatarBubble = ({ config, size = 32, style }: AvatarBubbleProps) => (
+  <View
+    style={[
+      styles.container,
+      {
+        width: size,
+        height: size,
+        borderRadius: size / 2,
+        backgroundColor: config.color,
+      },
+      style,
+    ]}
+  >
+    <Text style={styles.emoji}>{config.emoji}</Text>
+  </View>
+);
+
+const styles = StyleSheet.create({
+  container: {
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  emoji: {
+    fontSize: 20,
+  },
+});
+
+export default AvatarBubble;
