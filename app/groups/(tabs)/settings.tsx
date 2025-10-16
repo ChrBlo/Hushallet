7d888a3df312795dc92f217420c535be9f65708b
SophWennersten <sophwennersten@hotmail.com>
2025-10-16 15:23:43 +0200

    Fixed styling on ScrollView

--- app/groups/(tabs)/settings.tsx
+++ app/groups/(tabs)/settings.tsx
@@ -1,10 +1,24 @@
-import { router } from 'expo-router';
-import { Button, View, Text } from 'react-native';
+import { ScrollView, StyleSheet, Text } from 'react-native';
+import { MD3Theme, useTheme } from 'react-native-paper';
 
 export default function StatisticsScreen() {
+  const theme = useTheme();
+  const s = createStyles(theme);
   return (
-    <View>
+    <ScrollView style={s.scrollView} contentContainerStyle={s.container}>
       <Text>Inställningar</Text>
-    </View>
+    </ScrollView>
   );
 }
+
+const createStyles = (theme: MD3Theme) =>
+  StyleSheet.create({
+    container: {
+      paddingTop: 10,
+      gap: 10,
+    },
+    scrollView: {
+      flex: 1,
+      backgroundColor: theme.colors.background,
+    },
+  });
